1e6f4f829ecbbf3dbf2a204531d7976c757905e5
IbrahimAhmed <ibrahimmido580@gmail.com>
2022-09-20 16:00:31 +0200

    first

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+////$(function () {
+
+////    'use strict';
+
+////    $('.header').height($(window).height());
+
+////    $(window).resize(function () {
+////        $('.header').header($(window).height());
+
+
+   
+
+////    });
+////});
+
+ 
+
+//  $('.ser').click(function () {
+
+//    $('html, body').animate({
+
+//        scrollTop: $('#services').offfset().top
+
+//    }, 1000);
+
+//});
+
+
+
+
+
+
+
+
+
+
